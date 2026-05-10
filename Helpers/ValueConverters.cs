a61dbcf0b741d87b236bcf9e73d9bade6fa791d7
ikaken <7675846+ikaken@users.noreply.github.com>
2026-05-11 02:31:43 +0900

    fix: SettingsWindowのアップデート確認機能におけるConverterのリソース参照を修正

--- Helpers/ValueConverters.cs
+++ Helpers/ValueConverters.cs
@@ -16,4 +16,21 @@ namespace ChromeProfileLauncher.Helpers
             throw new NotImplementedException();
         }
     }
+
+    public class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolValue)
+            {
+                return !boolValue;
+            }
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
